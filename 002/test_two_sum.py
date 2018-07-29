b6cb351535021e290cde1cb5e3ec912bb58e1c47
CaiZhongheng1987 <38467143+CaiZhongheng1987@users.noreply.github.com>
2018-07-29 21:05:01 +0800

    Update test_two_sum.py

--- 002/test_two_sum.py
+++ 002/test_two_sum.py
@@ -13,15 +13,33 @@
 
 
 def two_sum(nums, target):
-    for first_idx in range(0, len(nums) - 1):
-        for second_idx in range(first_idx + 1, len(nums)):
-            if target == (nums[first_idx] + nums[second_idx]):
-                return [first_idx, second_idx]
+    # 使用字典，一遍for循环
+    new_dict = {}
+    for idx in range(0, len(nums)):
+        if (target - nums[idx]) in new_dict.keys() and new_dict[target - nums[idx]] != idx:
+            return [idx, new_dict[target - nums[idx]]]
+        else:
+            new_dict[nums[idx]] = idx
+
+    # 使用字典，两遍for循环
+    # new_dict = {}
+    # for idx in range(0, len(nums)):
+    #     new_dict[nums[idx]] = idx
+    #
+    # for idx in range(0, len(nums)):
+    #     if (target - nums[idx]) in new_dict.keys() and new_dict[target - nums[idx]] != idx:
+    #         return [idx, new_dict[target - nums[idx]]]
+
+    # 穷举法
+    # for first_idx in range(0, len(nums) - 1):
+    #     for second_idx in range(first_idx + 1, len(nums)):
+    #         if target == (nums[first_idx] + nums[second_idx]):
+    #             return [first_idx, second_idx]
 
     return
 
 
-test_list = [3, 2, 4]
+test_list = [3,3,11,15]
 test_target = 6
 
 return_idx = two_sum(test_list, test_target)
