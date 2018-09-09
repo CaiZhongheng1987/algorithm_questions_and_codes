5cf5447be0c9f08d034c010e001578fdfb08ed57
CaiZhongheng1987 <38467143+CaiZhongheng1987@users.noreply.github.com>
2018-09-09 20:15:11 +0800

    Create test_Regular_Expression_Matching_dp.py

---
+++ leetcode010/test_Regular_Expression_Matching_dp.py
@@ -0,0 +1,74 @@
+# 给定一个字符串 (s) 和一个字符模式 (p)。实现支持 '.' 和 '*' 的正则表达式匹配。
+#
+# '.' 匹配任意单个字符。
+# '*' 匹配零个或多个前面的元素。
+# 匹配应该覆盖整个字符串 (s) ，而不是部分字符串。
+#
+# 说明:
+#
+# s 可能为空，且只包含从 a-z 的小写字母。
+# p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+# 示例 1:
+#
+# 输入:
+# s = "aa"
+# p = "a"
+# 输出: false
+# 解释: "a" 无法匹配 "aa" 整个字符串。
+# 示例 2:
+#
+# 输入:
+# s = "aa"
+# p = "a*"
+# 输出: true
+# 解释: '*' 代表可匹配零个或多个前面的元素, 即可以匹配 'a' 。因此, 重复 'a' 一次, 字符串可变为 "aa"。
+# 示例 3:
+#
+# 输入:
+# s = "ab"
+# p = ".*"
+# 输出: true
+# 解释: ".*" 表示可匹配零个或多个('*')任意字符('.')。
+# 示例 4:
+#
+# 输入:
+# s = "aab"
+# p = "c*a*b"
+# 输出: true
+# 解释: 'c' 可以不被重复, 'a' 可以被重复一次。因此可以匹配字符串 "aab"。
+# 示例 5:
+#
+# 输入:
+# s = "mississippi"
+# p = "mis*is*p*."
+# 输出: false
+
+
+# 采用动态规划来做字符串匹配
+class Solution(object):
+    def isMatch(self, text, pattern):
+        memo = {}
+
+        def dp(i, j):
+            if (i, j) not in memo:
+                if j == len(pattern):
+                    ans = i == len(text)
+                else:
+                    first_match = i < len(text) and pattern[j] in {text[i], '.'}
+                    if j+1 < len(pattern) and pattern[j+1] == '*':
+                        ans = dp(i, j+2) or first_match and dp(i+1, j)
+                    else:
+                        ans = first_match and dp(i+1, j+1)
+
+                memo[i, j] = ans
+            return memo[i, j]
+
+        return dp(0, 0)
+
+
+s_string = 'mississippi'
+p_string = "mis*is*p*."
+
+my_test = Solution()
+result = my_test.isMatch(s_string, p_string)
+print(result)
